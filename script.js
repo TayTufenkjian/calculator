c14127cbe5bf81b1667fb1888911346dd0429c91
TayTufenkjian <taytuf@gmail.com>
2022-05-11 12:14:37 -0400

    Fix display for extra large and small numbers
    Fixes an issue where numbers with lots of digits
    were overflowing the display area

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
     // Initialize values and operator
     let a = b = textValue = operator = '';
 
+    // Get the display div
+    const displayDiv = document.querySelector('#display');
+
     // Listen for a click on each button
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            // If the button is AC, clear all values
+            // If the button is AC, clear all values and reset font-size
             if (button.id === 'clear') {
                 a = b = textValue = operator = '';
+                displayDiv.style.fontSize = '8rem';
                 display('0');
             }
 
@@ -18,7 +22,14 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (button.textContent === '.' && textValue.includes('.')) {
                     textValue = textValue;
                 } else {
-                    textValue += button.textContent;
+                    // Restrict textValue length to 9 chars
+                    if (textValue.length < 9) {
+                        textValue += button.textContent;
+                    }
+                }
+                // If textValue contains more than 6 chars, reduce the display font-size
+                if (textValue.length > 6) {
+                    displayDiv.style.fontSize = '5rem';
                 }
                 display(textValue);
             }
@@ -33,6 +44,14 @@ document.addEventListener('DOMContentLoaded', () => {
                     if (textValue) {
                         b = textValue;
                         let solution = Math.round(operate(Number(a), operator, Number(b)) * 100000000) / 100000000;
+                        // If the solution contains more than 10 chars, convert to E notation
+                        if (solution.toString().length > 10) {
+                            solution = convertLargeNumber(solution);
+                        }
+                        // If the solution contains more than 6 chars, reduce the display font-size
+                        if (solution.toString().length > 6) {
+                            displayDiv.style.fontSize = '5rem';
+                        }
                         display(solution);
                         // Update a and reset b for the next calculation
                         a = solution;
@@ -87,4 +106,17 @@ function operate(a, operator, b) {
             console.log('Something went wrong');
             return 'error';
     }
+}
+
+// Convert and truncate a large number for cleaner display
+function convertLargeNumber(num) {
+    numString = num.toExponential();
+    if (numString.length > 10 ) {
+        let index = numString.indexOf('e');
+        let numPrefix = numString.slice(0, index);
+        let numSuffix = numString.slice(index);
+        let truncNumPrefix = numPrefix.slice(0, 10 - numSuffix.length);
+        numString = truncNumPrefix + numSuffix;
+    }
+    return numString;
 }
